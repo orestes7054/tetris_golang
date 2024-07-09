e711f6047cb81fc13cc39909c538fd868d573dfd
orestes7054 <orestes.om@gmail.com>
2024-07-09 15:13:02 -0300

    changed approach board and blocks

--- board/board.go
+++ board/board.go
@@ -8,13 +8,14 @@ import (
 	"os"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
+	//"tetris_golang/tetrominos"
 )
 
 
 const (
 	ScreenWidth  = 450
 	ScreenHeight = 650
-	BlockSize = 20
+	BlockSize = 50
 	BoardWidth = 10
 	BoardHeight = 20
 	Padding = 20
@@ -26,8 +27,9 @@ type Game struct {
 }
 
 var (
-	backGroundColor = color.RGBA{0, 0, 0, 255}
-	blockColor = color.RGBA{255,255,255,255}
+	backGroundColor = color.RGBA{64, 64, 64, 0}
+	blockColor = color.RGBA{255,128,0,255}
+	lineColor = color.RGBA{0,0,0,255}
 )
 
 
@@ -38,13 +40,20 @@ func (g *Game) Layout(outSideWidth, outSideHeigth int) (screenWidth, screenHeigh
 func (g *Game) Draw(screen *ebiten.Image){
 	screen.Fill(backGroundColor)
 	
-	for y := 0; y < BoardHeight; y++ {
-		for x := 0; x < BoardWidth; x++ {
-			if g.board[y][x] != 0 {
-				vector.DrawFilledRect(screen, float32(Padding+x*BlockSize), float32(Padding+y*BlockSize), float32(BlockSize), float32(BlockSize), blockColor, true)
-			}
+
+	vector.DrawFilledRect(screen,float32(0), float32(0), float32(BlockSize), float32(BlockSize), blockColor, true)
+	// the BlockSize * X moves the block two blocks to the right idem with BlockSize * Y
+	vector.DrawFilledRect(screen,float32(BlockSize * 2), float32(BlockSize), float32(BlockSize), float32(BlockSize), blockColor, true)
+
+	
+	for x := 0; x < BoardWidth; x++ {
+		for y := 0; y < BoardHeight; y++ {
+			vector.StrokeRect(screen, float32(x * BlockSize), float32(y * BlockSize), float32(BlockSize), float32(BlockSize), float32(0.50), lineColor, true)
 		}
 	}
+
+
+	
 
 
 	
--- tetrominos/tetrominos.go
+++ tetrominos/tetrominos.go
@@ -1,17 +1,28 @@
 package tetrominos
 
+type Point struct{
+	X, Y int
+}
+
 type Tetromino struct {
-	rotations [][][]int
-	color     int
+	Rotations [][]Point
+	Color     int
 }
 
 
-type Tetris struct{
-	tetronominos []Tetromino
+var Cube = Tetromino{
+	Rotations: [][]Point{
+		{
+            {0, 0}, {1, 0}, {0, 1}, {1, 1},
+        },
+	},
+	Color: 1,
 }
 
 
 
-	//TODO create one object for each piece 
+
+
+
 
 
